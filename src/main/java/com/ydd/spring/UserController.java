2cf42fba55b4745c7f5bef48b0dcb296f9774fc7
yindd <695685009@qq.com>
2019-08-17 22:02:49 +0800

    spring mvc指定根路径的表单数据提交

--- src/main/java/com/ydd/spring/UserController.java
+++ src/main/java/com/ydd/spring/UserController.java
@@ -4,11 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
 	
 	@RequestMapping("hello")
 	public String myHello(){
 		return "index";
 	}
+	
+	@RequestMapping("add")
+	public String myAdd(){
+		return "add";
+	}
+	
+	@RequestMapping("receiveInt")
+	public String receiveInt(Integer id){
+		System.out.println(id);
+		return "success";
+	}
+	
 
 }
