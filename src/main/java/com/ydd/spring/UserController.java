b148b141858d319634466e1b6192ffc2509b169c
yindd <695685009@qq.com>
2019-08-17 19:45:41 +0800

    注解springmvc

---
+++ src/main/java/com/ydd/spring/UserController.java
@@ -0,0 +1,14 @@
+package com.ydd.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("hello")
+	public String myHello(){
+		return "index";
+	}
+
+}
